dd938a9fd9b244a83a2705e3f271fc78382f33b9
Kalana Mihiranga <33614427+kmihiranga@users.noreply.github.com>
2022-11-30 11:26:14 +0530

    added new provider component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,18 @@
+import { useContext } from "react";
+import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
 function BookList({ books, onDelete, onEdit }) {
+    const { count, incrementCount } = useContext(BooksContext);
+
     const renderedBooks = books.map((book) => {
         return <BookShow onEdit={onEdit} key={book.id} book={book} onDelete={onDelete}/>
     });
 
     return (
         <div className="book-list">
+            {count}
+            <button onClick={incrementCount}>Increment</button>
             {renderedBooks}
         </div>
     );
