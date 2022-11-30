aca19311c6743c10ee5d56111e5c0940f855ed0f
Kalana Mihiranga <33614427+kmihiranga@users.noreply.github.com>
2022-11-30 15:20:14 +0530

    refactored with context and hooks

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,18 +1,15 @@
-import { useContext } from "react";
-import BooksContext from "../context/books";
+import useBooksContext from "../hooks/use-books-context";
 import BookShow from "./BookShow";
 
-function BookList({ books, onDelete, onEdit }) {
-    const { count, incrementCount } = useContext(BooksContext);
+function BookList() {
+    const { books } = useBooksContext();
 
     const renderedBooks = books.map((book) => {
-        return <BookShow onEdit={onEdit} key={book.id} book={book} onDelete={onDelete}/>
+        return <BookShow key={book.id} book={book} />
     });
 
     return (
         <div className="book-list">
-            {count}
-            <button onClick={incrementCount}>Increment</button>
             {renderedBooks}
         </div>
     );
